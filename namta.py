12a7cf7367766bc61d2699c1a4a6552745384797
mrarik7 <arikff007@gmail.com>
2025-11-06 18:44:50 +0600

    Add files via upload

---
+++ namta.py
@@ -0,0 +1,18 @@
+import streamlit as st
+st.header('এইখানে যেকোনো নাম্বার দিলে ১০ পর্যন্ত তার নামতা বের হবে :)')
+a = st.text_input('এইখানে আপনামড় নাম্বার টি দেনঃ')
+if a :
+    a=int(a)
+if a:
+    st.text(f'{a} এর নামতা নিম্নরুপঃ')
+    b=0
+    while b<=9:
+        b=b+1
+        c=a*b
+        st.text(f'{a}   X   {b}   =   {c}')
+
+
+
+
+
+st.text('-mr.arik7')
